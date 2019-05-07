120f089d21b1318464b2665d2b4f638c7dd7af57
Mats Tuovinen <mats.tuovinen@stabelo.se>
2019-05-07 09:39:22 +0200

    Fix css modules.

--- frontend/application/components/header/Header.tsx
+++ frontend/application/components/header/Header.tsx
@@ -15,18 +15,20 @@ class Header extends React.Component<IProperties> {
             location
         } = this.props;
 
+        const classes = [css.header];
+
         return (
-            <header className="header">
+            <header className={classes.join(" ")}>
                 <nav>
                     <ul>
 
                         <a href="https://stabelo.se" ><img src="https://s3.eu-central-1.amazonaws.com/static.stabelo.net.eu-central-1/Stabelo_logo_RGB.png"  alt="Stabelo" /></a>
 
                         <li>
-                            <Link className={location.pathname === "/about" ? "active" : ""} to="/about">Om uppgiften</Link>
+                            <Link className={location.pathname === "/about" ? css.active : ""} to="/about">Om uppgiften</Link>
                         </li>
                         <li>
-                            <Link className={location.pathname === "/implementation" ? "active" : ""} to="/implementation">Implementation</Link>
+                            <Link className={location.pathname === "/implementation" ? css.active : ""} to="/implementation">Implementation</Link>
                         </li>
                     </ul>
                 </nav>
